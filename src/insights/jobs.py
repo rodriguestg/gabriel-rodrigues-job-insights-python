5e21b97407cd28810e16fb6742b93b61f0ed301b
rodriguestg <grs.ibr@gmail.com>
2023-03-03 17:24:26 -0300

    testando req 1

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,9 +1,18 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
+    with open(path, mode="r", encoding="utf-8") as file:
+        jobs_csv = csv.DictReader(file, delimiter=",", quotechar='"')
+        dict_jobs = [jobs for jobs in jobs_csv]
+        # print(jobs_cs)
+    return dict_jobs
+
+        # jobs = csv.reader(file, delimiter=',')
+        # print(jobs)
     """Reads a file from a given path and returns its contents
 
     Parameters
