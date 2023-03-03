4ba974e7bd6a2df532b10d00a3271caa9f772bbb
rodriguestg <grs.ibr@gmail.com>
2023-03-03 19:21:41 -0300

    Requisito 3, recebendo as industrias dentro do csv sem duplicação

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,7 +1,17 @@
 from typing import List, Dict
+import csv
 
 
 def get_unique_industries(path: str) -> List[str]:
+    with open(path, mode="r", encoding="utf-8") as file:
+        industries_csv = csv.DictReader(file, delimiter=",", quotechar='"')
+        dict_industries = [industries for industries in industries_csv]
+    group_by_industries = []
+    for row in dict_industries:
+        industries_row = row["industries"]
+        if industries_row not in group_by_industries:
+            group_by_industries.append(row)
+    return group_by_industries
     """Checks all different industries and returns a list of them
 
     Must call `read`
